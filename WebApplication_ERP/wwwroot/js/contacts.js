feb0c3f8333ab88237e2b799ccb4619e4d6b1a35
Louis boeckmans <louis.boeckmans@outlook.com>
2023-10-25 22:16:45 +0200

    main

---
+++ WebApplication_ERP/wwwroot/js/contacts.js
@@ -0,0 +1,10 @@
+﻿
+//Redimensionne les divs dans lequel se trouve les boutons ajouter en fonction des tableaux au-dessus
+$(document).ready(function () {
+
+    var tableWidth = $("#contacts-table").width();
+    $("#right-button-div-contact").css("width", tableWidth + "px");
+
+    tableWidth = $("#organisation-table").width();
+    $("#right-button-div-organisation").css("width", tableWidth + "px");
+});
